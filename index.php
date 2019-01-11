3792303ba0aed6cc9f31c6a15da52fce7c48c7d9
Davide <pzzdave95@gmail.com>
2019-01-11 13:20:03 +0000

    Changes                                                           - Changes to have the homepage background based on the whether forecast

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once('Models/ConnectionConsumption.php');
-require_once ('Models/ConnectionWeather');
+require_once('Models/ConnectionWeather.php');
 require_once ('Models/ConnectionLocation.php');
 $view = new stdClass();
 $view->pageTitle = 'Homepage';
@@ -17,10 +17,10 @@ $view->totalGas = $gasDataSet->getGasCost();
 $url = new ConnectionLocation();
 $location = $url->getData();
 //changes the url for the weather api
-$url = new ConnectionWeather('forecast', $location->getCountry());
+$url = new php('forecast', $location->getCountry());
 // weather for 5 days in the future
 $view->weatherPredictionSet = $url->getData('forecast');
-$url = new ConnectionWeather('weather',$location->getCountry());
+$url = new php('weather',$location->getCountry());
 // weather for the current time of access to the app
 $view->weatherNow = ($url->getData('weather'))->getWeatherArray()[0];
 
