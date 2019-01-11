37d04eea3972a48c406cb0c2a6716d18aa02bbf1
Haashim <compsoc@firstyearmatters.info>
2019-01-11 11:28:03 +0000

    Added javascript to change the colour based on budget

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
 require_once('Models/ConnectionConsumption.php');
 require_once ('Models/ConnectionWeather');
+require_once ('Models/ConnectionLocation.php');
 $view = new stdClass();
 $view->pageTitle = 'Homepage';
-// for backend testing
-//echo '<form action="index.php" method="post">';
-//echo '<input type="submit" name="ELEC" value="ELEC">';
-//echo '<input type="submit" name="GAS" value="GAS">';
-//echo '</form>';
-//
-//if (isset($_POST)) {
-//    $data = array('period' => 'PT1H');
-//    if (isset($_POST['ELEC'])) {
-//        $data['type'] = 'ELEC';
-//    } else {
-//        $data['type'] = 'GAS';
-//    }
-
 
 $elecConsum = new ConnectionConsumption('ELEC','PT1H');
 $elecDataSet = $elecConsum->getData();
@@ -25,28 +12,16 @@ $gasConsum = new ConnectionConsumption('GAS','PT1H');
 $gasDataSet = $gasConsum->getData();
 
 $view->totalElec = $elecDataSet->getElecCost();
-//var_dump($view->totalElec);
-//die();
 $view->totalGas = $gasDataSet->getGasCost();
-require_once('Views/index.phtml');
-//    if (isset($_POST['ELEC'])) {
-//        echo 'Total Electricity Usage:' . $dataSet->getElecCost() . '£';
-//        echo "<br/>";
-//        echo 'Total Electricity Consumption this month: ' . $dataSet->getTotalConsumption() . ' kWh';
-//        echo "<br/>";
-//    } else {
-//        echo 'Total Gas Usage:' . $dataSet->getGasCost() . '£';
-//        echo "<br/>";
-//        echo 'Total Gas Consumption this month: ' . $dataSet->getTotalConsumption() . ' kWh';
-//        echo "<br/>";
-//    }
 
+$url = new ConnectionLocation();
+$location = $url->getData();
+//changes the url for the weather api
+$url = new ConnectionWeather('forecast', $location->getCountry());
+// weather for 5 days in the future
+$view->weatherPredictionSet = $url->getData('forecast');
+$url = new ConnectionWeather('weather',$location->getCountry());
+// weather for the current time of access to the app
+$view->weatherNow = ($url->getData('weather'))->getWeatherArray()[0];
 
-
-// changes the url for the weather api
-//    $url = new ConnectionWeather();
-//    $dataSet = $url->getData();
-//    echo '<p> Forecast ' . $dataSet->getWeatherArray()[0]->getForecast() . '</p>';
-
-
-//}
+require_once ('Views/index.phtml');
