f115d5bc12e73868c9877180622d5e976209a94d
Omar <haashimalvi@protonmail.com>
2019-01-14 10:45:54 +0000

    made database class

---
+++ Models/Database.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "group2.edu.csesalford.com";
+$username = "group2";
+$password = "SWLWmTppbPUQpzX";
+$db = "group2";
+
+$conn = new PDO("mysql:host=$servername;dbname=$db",$username,$password);
+
+?>
