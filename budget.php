69e5345035e16373fb50960ac66bda5dc41a662a
Abdi <compsoc@firstyearmatters.info>
2019-01-15 13:14:49 +0000

    Changes - made the budget more simple and worked on validation - got rid of test.php and made the link budget.php

---
+++ budget.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once("Models/Database.php");
+require_once("Models/BudgetDb.php");
+session_start();
+
+if (isset($_SESSION['error'])) {
+    unset($_SESSION['error']);
+}
+
+if (isset($_POST["submit"])) {
+    $emailValidation = $_POST['email'];
+    $electrictyValidation = $_POST['electricityPrice'];
+    $gasValidation = $_POST['gasPrice'];
+    if ($emailValidation == 'group2@hotmail.com') {
+        $data = array(
+            "electricityPrice" => $_POST["electricityPrice"],
+            "gasPrice" => $_POST["gasPrice"],
+            "email" => $_POST["email"],
+            "date" => $_POST["day"] . "-" . $_POST["month"] . "-" . $_POST["year"]
+        );
+        $budgetDatabase = new BudgetDb();
+        $budgetDatabase->insert($data);
+        $budget = $budgetDatabase->getBudget("group2@hotmail.com");
+        $_SESSION["electricityBudget"] = $budget["electricityPrice"];
+        $_SESSION["gasBudget"] = $budget["gasPrice"];
+        require_once("Views/seeBudget.phtml");
+    } else {
+        $error = 'Email is incorrect';
+        require_once("Views/budget.phtml");
+    }
+
+} else {
+    $budgetDatabase = new BudgetDb();
+    $budget2 = $budgetDatabase->getBudget("group2@hotmail.com");
+    $_SESSION["electricityBudget"] = $budget2["electricityPrice"];
+    $_SESSION["gasBudget"] = $budget2["gasPrice"];
+    require_once("Views/budget.phtml");
+    echo 'not working';
+}
+?>
