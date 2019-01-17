e6635ff3d94e7914ae7f082d60f4f41b12e9dffd
Dave <pzzdave95@gmail.com>
2019-01-17 20:49:21 +0000

    Refactoring to avoid repetition of code in the charts. The script drawChart() is now fused into a single one that can be used by all charts (predicted, compare and classical)

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 require_once('Models/ConnectionConsumption.php');
-require_once ('Models/Prediction.php');
-require_once('Models/ConnectionWeather.php');
-require_once ('Models/ConnectionLocation.php');
+//require_once ('Models/Prediction.php');
+//require_once('Models/ConnectionWeather.php');
+//require_once ('Models/ConnectionLocation.php');
 require_once ('Models/BudgetDb.php');
 
 session_start();
 $view = new stdClass();
 $view->pageTitle = 'Homepage';
+$view->type='BOTH';
 
 $elecConsum = new ConnectionConsumption('ELEC','PT1H', 'first day of this month 00:00:00', 'now');
 $elecDataSet = $elecConsum->getData();
@@ -18,38 +19,39 @@ $gasDataSet = $gasConsum->getData();
 $view->totalElec = $elecDataSet->getElecCost();
 $view->totalGas = $gasDataSet->getGasCost();
 
-$url = new ConnectionLocation();
-$location = $url->getData();
-//changes the url for the weather api
-$url = new ConnectionWeather('forecast', 'Salford');
-// weather for 5 days in the future
-$view->weatherPredictionSet = ($url->getData('forecast'))->getPredictedWeather();
-// predicted weather converted to a usable array for k nn
-$testWeatherArray = ($url->getData('forecast'))->createTestWeatherArray();
-$url = new ConnectionWeather('weather','Salford');
-// weather for the current time of access to the app
-$view->weatherNow = ($url->getData('weather'))->getWeatherArray()[0];
-
-// classifier initialised
-$prediction = new Prediction(7);
-// classifier trained on the training data set
-$prediction->train('Elec');
-// predicted usage for the 5 days saved for the view
-$view->predictedUsageElec = [];
-$count = 0;
-foreach ($testWeatherArray as $value) {
-    $view->predictedUsageElec[] = new ConsumptionData($view->weatherPredictionSet[$count]->getTime()->getTimeStamp(),
-        floatval($prediction->predict(array($value[0], $value[1], $value[2]))));
-}
-$prediction->train('Gas');
-// predicted usage for the 5 days saved for the view
-$view->predictedUsageGas = [];
-$count = 0;
-foreach ($testWeatherArray as $value) {
-    $view->predictedUsageGas[] = new ConsumptionData($view->weatherPredictionSet[$count]->getTime()->getTimeStamp(),
-        floatval($prediction->predict(array($value[0], $value[1], $value[2]))));
-    $count++;
-}
+////!!!! WEATHER MOVED TO   /Models/Request.php
+//$url = new ConnectionLocation();
+//$location = $url->getData();
+////changes the url for the weather api
+//$url = new ConnectionWeather('forecast', 'Salford');
+//// weather for 5 days in the future
+//$view->weatherPredictionSet = ($url->getData('forecast'))->getPredictedWeather();
+//// predicted weather converted to a usable array for k nn
+//$testWeatherArray = ($url->getData('forecast'))->createTestWeatherArray();
+//$url = new ConnectionWeather('weather','Salford');
+//// weather for the current time of access to the app
+//$view->weatherNow = ($url->getData('weather'))->getWeatherArray()[0];
+//
+//// classifier initialised
+//$prediction = new Prediction(7);
+//// classifier trained on the training data set
+//$prediction->train('Elec');
+//// predicted usage for the 5 days saved for the view
+//$view->predictedUsageElec = [];
+//$count = 0;
+//foreach ($testWeatherArray as $value) {
+//    $view->predictedUsageElec[] = new ConsumptionData($view->weatherPredictionSet[$count]->getTime()->getTimeStamp(),
+//        floatval($prediction->predict(array($value[0], $value[1], $value[2]))));
+//}
+//$prediction->train('Gas');
+//// predicted usage for the 5 days saved for the view
+//$view->predictedUsageGas = [];
+//$count = 0;
+//foreach ($testWeatherArray as $value) {
+//    $view->predictedUsageGas[] = new ConsumptionData($view->weatherPredictionSet[$count]->getTime()->getTimeStamp(),
+//        floatval($prediction->predict(array($value[0], $value[1], $value[2]))));
+//    $count++;
+//}
 
 
 // Doughnut Chart and colour classes
